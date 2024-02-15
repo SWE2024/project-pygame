ced24ff36fa7cf99519ef8215abf5aca7055e78d
Lewis <lewisryework@gmail.com>
2024-02-15 13:07:28 +0000

    new background

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ display_width = display_info.current_w
 display_height = display_info.current_h
 
 # choose here whether to start in resizable or fullscreen
-screen = pygame.display.set_mode((display_width, display_height), pygame.FULLSCREEN, vsync = 0)
+screen = pygame.display.set_mode((display_width, display_height), pygame.FULLSCREEN, vsync=0)
 # screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync = 0) # use to begin in windowed
 
-init_fullscreen = True # set to true if beginning in fulscreen
+init_fullscreen = True # set to true if beginning in fullscreen
 
 # file system setup
 current_path = os.path.dirname(__file__)
@@ -35,7 +35,7 @@ btnSettings = pygame.image.load(current_path + '/assets/btnSettings.png').conver
 btnSettingsHover = pygame.image.load(current_path + '/assets/btnSettingsHover.png').convert_alpha()
 btnAudio = pygame.image.load(current_path + '/assets/btnAudio.png').convert_alpha()
 btnAudioHover = pygame.image.load(current_path + '/assets/btnAudioHover.png').convert_alpha()
-btnBack= pygame.image.load(current_path + '/assets/btnBack.png').convert_alpha()
+btnBack = pygame.image.load(current_path + '/assets/btnBack.png').convert_alpha()
 btnBackHover = pygame.image.load(current_path + '/assets/btnBackHover.png').convert_alpha()
 imgLogo = pygame.image.load(current_path + '/assets/imgLogo.png').convert_alpha()
 imgBackground = pygame.image.load(current_path + '/assets/imgBackground.jpg').convert()
@@ -47,57 +47,61 @@ dt = 0
 """
 GAME CODE
 """
+
+
 def change_music(music_path):
     mixer.music.unload()
     mixer.music.load(music_path)
     mixer.music.play(-1)
     mixer.music.set_volume(0.3)
 
+
 class FPS:
     def __init__(self):
         self.font = pygame.font.SysFont(pygame.font.get_default_font(), 32)
-    
+
     def render(self, screen):
         self.text = self.font.render('FPS: ' + str(round(clock.get_fps())), True, (255, 255, 255))
         screen.blit(self.text, (0, 0))
 
+
 class UI:
-    #this variable holds the method to render the current scene
-    #the variable is a pointer to the actual method definitions defined below
-    #to change scene, set this variable to the scene's method and return out of the current method
-    #the while loop will run the next scene's method true this variable
+    # this variable holds the method to render the current scene
+    # the variable is a pointer to the actual method definitions defined below
+    # to change scene, set this variable to the scene's method and return out of the current method
+    # the while loop will run the next scene's method true this variable
     current_page = None
 
-    #change this value based on the starting screen
+    # change this value based on the starting screen
     fullscreen = None
-    #change which key activates the fullscreen and resizable screen
+    # change which key activates the fullscreen and resizable screen
     fullscreen_key = pygame.K_f
 
     def __init__(self):
-        self.font = pygame.font.SysFont("kalinga", 32)
+        self.font = pygame.font.SysFont("Arial", 32)
         self.current_page = self.menu
         self.fullscreen = init_fullscreen
 
         # load startup music
         change_music(current_path + '/assets/musicMenu.mp3')
-    
+
     def render(self, screen):
         self.text = self.font.render('TAB to LEAVE | ESC to QUIT', True, (255, 255, 255))
         self.text_width = self.text.get_width()
         screen.blit(self.text, (screen.get_width() - self.text_width, 0))
         self.icon = pygame.transform.scale(icon, (50, 50))
-        screen.blit(self.icon, (5, screen.get_height() - 55)) # offset by 5px so it is not stuck in the bottom left
+        screen.blit(self.icon, (5, screen.get_height() - 55))  # offset by 5px, so it is not stuck in the bottom left
 
-    #this the method to render the menu scene
+    # this the method to render the menu scene
     def menu(self):
-        #the screen and running variable are defined global
-        #the initialization of these variables do not exist in this scope rather outside it.
+        # the screen and running variable are defined global
+        # the initialization of these variables do not exist in this scope rather outside it.
         global screen, running
 
         current_background = pygame.transform.scale(imgBackground, (screen.get_width(), screen.get_height()))
 
         while 1:
-            dt = clock.tick(165) * 0.001 # limit fps to 165 in game
+            dt = clock.tick(165) * 0.001  # limit fps to 165 in game
 
             screen.blit(current_background, (0, 0))
 
@@ -109,7 +113,8 @@ class UI:
             areaExitBtn = pygame.Rect(screen.get_width() * 0.5 - 152.5, screen.get_height() * 0.5 + 47.5, 305, 95)
             screen.blit(btnExit, (screen.get_width() * 0.5 - 152.5, screen.get_height() * 0.5 + 47.5))
 
-            areaSettingsBtn = pygame.Rect(screen.get_width() - 144 - 10, 10, 144, 122) # offset 10px from the edge of the screen
+            areaSettingsBtn = pygame.Rect(screen.get_width() - 144 - 10, 10, 144,
+                                          122)  # offset 10px from the edge of the screen
             screen.blit(btnSettings, (screen.get_width() - 144 - 10, 10))
 
             cursor_pos = pygame.mouse.get_pos()
@@ -122,15 +127,9 @@ class UI:
 
             keys = pygame.key.get_pressed()
 
-            #this is the example to switch to the game scene
-            # i just used the p key for example
-            if keys[pygame.K_p]:
-                #you first change the current_page variable to the next scene you want to render
-                self.current_page = self.game
-                return                                
-
-            #when quitting the game, you just set running to false and return out the while loop will terminate the game
-            elif keys[pygame.K_ESCAPE]:
+            # when quitting the game, set running to false and returning out
+            # of the while loop will terminate the game
+            if keys[pygame.K_ESCAPE]:
                 running = False
                 return
 
@@ -138,7 +137,7 @@ class UI:
                 if event.type == pygame.QUIT:
                     running = False
                     return
-                
+
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if areaPlayBtn.collidepoint(event.pos):
                         self.current_page = self.game
@@ -150,7 +149,7 @@ class UI:
                         screen.fill("black")
                         current_background.set_alpha(60)
                         screen.blit(current_background, (0, 0))
-                        pygame.display.flip() 
+                        pygame.display.flip()
                         """
                         ! important !
                         this lowers the opacity of the background BEFORE opening the menu
@@ -161,23 +160,25 @@ class UI:
                         return
 
                 if event.type == pygame.KEYDOWN and event.key == self.fullscreen_key:
-                    if self.fullscreen: 
-                        screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync = 0)
-                        current_background = pygame.transform.scale(imgBackground, (screen.get_width(), screen.get_height()))
+                    if self.fullscreen:
+                        screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync=0)
+                        current_background = pygame.transform.scale(imgBackground,
+                                                                    (screen.get_width(), screen.get_height()))
                         self.fullscreen = False
-                    else: 
-                        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, vsync = 0)
-                        current_background = pygame.transform.scale(imgBackground, (screen.get_width(), screen.get_height()))
+                    else:
+                        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, vsync=0)
+                        current_background = pygame.transform.scale(imgBackground,
+                                                                    (screen.get_width(), screen.get_height()))
                         self.fullscreen = True
-            
+
             fps.render(screen)
             pygame.display.flip()
-    
+
     def menu_settings(self):
         global screen, running
 
         while 1:
-            dt = clock.tick(165) * 0.001 # limit fps to 165 in game
+            dt = clock.tick(165) * 0.001  # limit fps to 165 in game
 
             areaAudioBtn = pygame.Rect(screen.get_width() - 289 - 5, 132, 289, 90)
             screen.blit(btnAudio, (screen.get_width() - 289 - 5, 132))
@@ -185,7 +186,8 @@ class UI:
             areaBackBtn = pygame.Rect(screen.get_width() - 289 - 5, 222, 289, 90)
             screen.blit(btnBack, (screen.get_width() - 289 - 5, 222))
 
-            areaSettingsBtn = pygame.Rect(screen.get_width() - 144 - 10, 10, 144, 122) # offset 10px from the edge of the screen
+            areaSettingsBtn = pygame.Rect(screen.get_width() - 144 - 10, 10, 144,
+                                          122)  # offset 10px from the edge of the screen
             screen.blit(btnSettings, (screen.get_width() - 144 - 10, 10))
 
             cursor_pos = pygame.mouse.get_pos()
@@ -209,35 +211,37 @@ class UI:
                         else:
                             mixer.music.set_volume(0.33)
                         return
-                    
+
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if areaBackBtn.collidepoint(event.pos) or areaSettingsBtn.collidepoint(event.pos):
                         self.current_page = self.menu
                         return
-                    
+
                 if event.type == pygame.KEYDOWN and event.key == self.fullscreen_key:
-                    if self.fullscreen: 
-                        screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync = 0)
-                        current_background = pygame.transform.scale(imgBackground, (screen.get_width(), screen.get_height()))
+                    if self.fullscreen:
+                        screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync=0)
+                        current_background = pygame.transform.scale(imgBackground,
+                                                                    (screen.get_width(), screen.get_height()))
                         current_background.set_alpha(60)
                         screen.blit(current_background, (0, 0))
-                        pygame.display.flip() 
+                        pygame.display.flip()
                         """
                         ! important !
-                        this lowers the opacity of the background BEFORE fullscreening
+                        this lowers the opacity of the background BEFORE fullscreen
                         this means you do not need to call .set_alpha(60) on each frame
                         while in the menu, which tanks performance
                         """
                         self.fullscreen = False
-                    else: 
-                        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, vsync = 0)
-                        current_background = pygame.transform.scale(imgBackground, (screen.get_width(), screen.get_height()))
+                    else:
+                        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, vsync=0)
+                        current_background = pygame.transform.scale(imgBackground,
+                                                                    (screen.get_width(), screen.get_height()))
                         current_background.set_alpha(60)
                         screen.blit(current_background, (0, 0))
-                        pygame.display.flip() 
+                        pygame.display.flip()
                         """
                         ! important !
-                        this lowers the opacity of the background BEFORE fullscreening
+                        this lowers the opacity of the background BEFORE fullscreen
                         this means you do not need to call .set_alpha(60) on each frame
                         while in the menu, which tanks performance
                         """
@@ -246,7 +250,7 @@ class UI:
             """
             render only the button area, to improve performance and reduce unnecessary rendering
             """
-            pygame.draw.rect(screen, (0, 0, 0, 255), pygame.Rect(0, 0, 100, 25)) # prevents FPS values overlapping
+            pygame.draw.rect(screen, (0, 0, 0, 255), pygame.Rect(0, 0, 100, 25))  # prevents FPS values overlapping
             fps.render(screen)
             pygame.display.update(pygame.Rect(0, 0, 100, 25))
             pygame.display.update(pygame.Rect(screen.get_width() - 289 - 10, 0, 304, 322))
@@ -262,7 +266,7 @@ class UI:
         while 1:
             screen.fill("black")
 
-            dt = clock.tick(165) * 0.001 # limit fps to 165 in game
+            dt = clock.tick(165) * 0.001  # limit fps to 165 in game
 
             pygame.draw.circle(screen, "red", player_pos, 33)
 
@@ -276,15 +280,15 @@ class UI:
                 player_pos.x -= 500 * dt
             if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and player_pos.x < screen.get_width():
                 player_pos.x += 500 * dt
-            if (keys[pygame.K_TAB]):
+            if keys[pygame.K_TAB]:
                 self.current_page = self.menu
                 # load menu music
                 change_music(current_path + '/assets/musicMenu.mp3')
                 return
-            if (keys[pygame.K_ESCAPE]):
+            if keys[pygame.K_ESCAPE]:
                 running = False
                 return
-            
+
             # flip() the display to put your work on screen
             fps.render(screen)
             ui.render(screen)
@@ -293,16 +297,17 @@ class UI:
                 if event.type == pygame.QUIT:
                     running = False
                     return
-                
+
                 if event.type == pygame.KEYDOWN and event.key == self.fullscreen_key:
-                    if self.fullscreen: 
-                        screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync = 0)
+                    if self.fullscreen:
+                        screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync=0)
                         self.fullscreen = False
-                    else: 
-                        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, vsync = 0)
+                    else:
+                        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, vsync=0)
                         self.fullscreen = True
 
             pygame.display.flip()
+
 
 fps = FPS()
 ui = UI()
