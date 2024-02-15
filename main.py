38319d2e503d83226831c1fe53d3a571b5498919
Lewis <lewisryework@gmail.com>
2024-02-15 14:20:52 +0000

    added game settings menu

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ display_info = pygame.display.Info()
 display_width = display_info.current_w
 display_height = display_info.current_h
 
-# choose here whether to start in resizable or fullscreen
+# choose here whether to start in resizable or full screen
 screen = pygame.display.set_mode((display_width, display_height), pygame.FULLSCREEN, vsync=0)
 # screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync = 0) # use to begin in windowed
 
-init_fullscreen = True # set to true if beginning in fullscreen
+init_fullscreen = True # set to true if beginning in full screen
 
 # file system setup
 current_path = os.path.dirname(__file__)
@@ -60,7 +60,7 @@ def change_music(music_path):
 
 class FPS:
     def __init__(self):
-        self.font = pygame.font.SysFont(pygame.font.get_default_font(), 32)
+        self.font = pygame.font.SysFont('Calibri', 32)
 
     def render(self, screen):
         self.text = self.font.render('FPS: ' + str(round(clock.get_fps())), True, (255, 255, 255))
@@ -68,10 +68,12 @@ class FPS:
 
 
 class UI:
-    # this variable holds the method to render the current scene
-    # the variable is a pointer to the actual method definitions defined below
-    # to change scene, set this variable to the scene's method and return out of the current method
-    # the while loop will run the next scene's method true this variable
+    """
+    this variable holds the method to render the current scene
+    the variable is a pointer to the actual method definitions defined below
+    to change scene, set this variable to the scene's method and return out of
+    the current method to switch
+    """
     current_page = None
 
     # change this value based on the starting screen
@@ -80,7 +82,7 @@ class UI:
     fullscreen_key = pygame.K_f
 
     def __init__(self):
-        self.font = pygame.font.SysFont("Arial", 32)
+        self.font = pygame.font.SysFont('Calibri', 32)
         self.current_page = self.menu
         self.fullscreen = init_fullscreen
 
@@ -88,9 +90,9 @@ class UI:
         change_music(current_path + '/assets/musicMenu.mp3')
 
     def render(self, screen):
-        self.text = self.font.render('TAB to LEAVE | ESC to QUIT', True, (255, 255, 255))
-        self.text_width = self.text.get_width()
-        screen.blit(self.text, (screen.get_width() - self.text_width, 0))
+        """
+        render all game UI required here
+        """
         self.icon = pygame.transform.scale(icon, (50, 50))
         screen.blit(self.icon, (5, screen.get_height() - 55))  # offset by 5px, so it is not stuck in the bottom left
 
@@ -143,6 +145,7 @@ class UI:
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if areaPlayBtn.collidepoint(event.pos):
                         self.current_page = self.game
+                        change_music(current_path + '/assets/musicBackground.mp3')
                         return
                     if areaQuitBtn.collidepoint(event.pos):
                         running = False
@@ -213,8 +216,6 @@ class UI:
                         else:
                             mixer.music.set_volume(0.33)
                         return
-
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if areaBackBtn.collidepoint(event.pos) or areaSettingsBtn.collidepoint(event.pos):
                         self.current_page = self.menu
                         return
@@ -260,28 +261,26 @@ class UI:
     def game(self):
         global running, screen
 
-        player_pos = pygame.Vector2(screen.get_width() * 0.5, screen.get_height() * 0.5)
-
-        # load game music
-        change_music(current_path + '/assets/musicBackground.mp3')
-
         while 1:
             screen.fill("black")
-
             dt = clock.tick(165) * 0.001  # limit fps to 165 in game
 
-            pygame.draw.circle(screen, "red", player_pos, 33)
+            # insert game logic here
+            pygame.draw.rect(screen, "red", (100, 100, 100, 100))
+
+            pygame.draw.rect(screen, "green", (300, 100, 100, 100))
+
+            pygame.draw.rect(screen, "blue", (500, 100, 100, 100))
+
+            areaSettingsBtn = pygame.Rect(screen.get_width() - 144 - 10, 10, 144,
+                                          122)  # offset 10px from the edge of the screen
+            screen.blit(btnSettings, (screen.get_width() - 144 - 10, 10))
+            cursor_pos = pygame.mouse.get_pos()
+            if areaSettingsBtn.collidepoint(cursor_pos):
+                screen.blit(btnSettingsHover, (screen.get_width() - 144 - 10, 10))
 
             keys = pygame.key.get_pressed()
 
-            if (keys[pygame.K_w] or keys[pygame.K_UP]) and player_pos.y > 0:
-                player_pos.y -= 500 * dt
-            if (keys[pygame.K_s] or keys[pygame.K_DOWN]) and player_pos.y < screen.get_height():
-                player_pos.y += 500 * dt
-            if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and player_pos.x > 0:
-                player_pos.x -= 500 * dt
-            if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and player_pos.x < screen.get_width():
-                player_pos.x += 500 * dt
             if keys[pygame.K_TAB]:
                 self.current_page = self.menu
                 # load menu music
@@ -299,6 +298,11 @@ class UI:
                 if event.type == pygame.QUIT:
                     running = False
                     return
+                
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if areaSettingsBtn.collidepoint(event.pos):
+                        self.current_page = self.game_settings
+                        return
 
                 if event.type == pygame.KEYDOWN and event.key == self.fullscreen_key:
                     if self.fullscreen:
@@ -309,11 +313,73 @@ class UI:
                         self.fullscreen = True
 
             pygame.display.flip()
+    
+    def game_settings(self):
+        global screen, running
+
+        while 1:
+            screen.fill('black')
+            dt = clock.tick(165) * 0.001  # limit fps to 165 in game
+
+            areaAudioBtn = pygame.Rect(screen.get_width() - 289 - 5, 132, 289, 90)
+            screen.blit(btnAudio, (screen.get_width() - 289 - 5, 132))
+
+            areaExitBtn = pygame.Rect(screen.get_width() - 289 - 5, 230, 289, 90)
+            screen.blit(btnExit, (screen.get_width() - 289 - 5, 230))
+
+            areaSettingsBtn = pygame.Rect(screen.get_width() - 144 - 10, 10, 144,
+                                          122)  # offset 10px from the edge of the screen
+            screen.blit(btnSettings, (screen.get_width() - 144 - 10, 10))
+
+            cursor_pos = pygame.mouse.get_pos()
+            if areaAudioBtn.collidepoint(cursor_pos):
+                screen.blit(btnAudioHover, (screen.get_width() - 289 - 5, 132))
+            elif areaExitBtn.collidepoint(cursor_pos):
+                screen.blit(btnExitHover, (screen.get_width() - 289 - 5, 230))
+            elif areaSettingsBtn.collidepoint(cursor_pos):
+                screen.blit(btnSettingsHover, (screen.get_width() - 144 - 10, 10))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    return
+
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if areaAudioBtn.collidepoint(event.pos):
+                        volume = mixer.music.get_volume()
+                        if volume > 0:
+                            mixer.music.set_volume(0)
+                        else:
+                            mixer.music.set_volume(0.33)
+                        return
+                    if areaExitBtn.collidepoint(event.pos):
+                        self.current_page = self.menu
+                        # load menu music
+                        change_music(current_path + '/assets/musicMenu.mp3')
+                        return
+                    if areaSettingsBtn.collidepoint(event.pos):
+                        self.current_page = self.game
+                        return
+
+                if event.type == pygame.KEYDOWN and event.key == self.fullscreen_key:
+                    if self.fullscreen:
+                        screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync=0)
+                        self.fullscreen = False
+                    else:
+                        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, vsync=0)
+                        self.fullscreen = True
+
+            """
+            render only the button area, to improve performance and reduce unnecessary rendering
+            """
+            pygame.draw.rect(screen, (0, 0, 0, 255), pygame.Rect(0, 0, 100, 25))  # prevents FPS values overlapping
+            fps.render(screen)
+            pygame.display.update(pygame.Rect(0, 0, 100, 25))
+            pygame.display.update(pygame.Rect(screen.get_width() - 289 - 10, 0, 304, 322))
 
 
 fps = FPS()
 ui = UI()
-
 running = True
 while running:
     ui.current_page()
